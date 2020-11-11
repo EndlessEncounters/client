607d329c7b46d83ea65ffb2afc948d24dbb30a69
John <jpending@gmail.com>
2020-11-11 18:30:46 -0500

    merge

--- src/services/event-service.js
+++ src/services/event-service.js
@@ -25,20 +25,7 @@ const EventService={
       },
       body: JSON.stringify({choice: choice})
     })
-    // return p1;
-    // return fetch(`${config.API_ENDPOINT}/choice`, {
-    //   method: 'POST',
-    //   headers: {
-    //     Authorization: `bearer ${TokenService.getAuthToken()}`,
-    //     'content-type': 'application/json'
-    //   },
-    //   body: JSON.stringify(ev)
-    // })
-    //   .then(res => {
-    //     return (!res.ok)
-    //       ? res.json().then(e => Promise.reject(e))
-    //       :res.json();
-    //   });
+
   }
 
 }
